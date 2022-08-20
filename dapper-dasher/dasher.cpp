e31645426fa81762b78019b9ded73e9ef6f1bc80
hairbearr <hbrock214@gmail.com>
2022-08-20 10:02:06 -0400

    implemented jumping rectangle

--- dapper-dasher/dasher.cpp
+++ dapper-dasher/dasher.cpp
@@ -2,12 +2,15 @@
 
 int main()
 {
-    // WINDOW DIMENSIONS
+    // WINDOW VARIABLES
     const int windowWidth{512}, windowHeight{450};
     const char * title = "Dapper Dasher!";
-
     // INITIALIZE THE WINDOW
     InitWindow(windowWidth, windowHeight, title);
+
+    // rectangle dimensions
+    const int width{50}, height{80};
+    int posY{windowHeight - height}, velocity{0};
 
 
 
@@ -17,7 +20,15 @@ int main()
         // START DRAWING WINDOW
         BeginDrawing();
         ClearBackground(WHITE);
-        
+
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
         
         
         
